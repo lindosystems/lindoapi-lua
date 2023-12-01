5b00b824f97244f88c043872fad04beb89e03385
Mustafa Atlihan <mkatlihan@gmail.com>
2023-12-01 03:16:50 +0300

    bins

--- activate_lindo_lua.sh
+++ activate_lindo_lua.sh
@@ -20,16 +20,19 @@ elif [ "${UNAME:0:7}" = "MSYS_NT" ]; then
     CURPATH=$(cygpath -w $CURPATH)
 elif [ "${UNAME}" = "Linux" ]; then
     PLATFORM=linux64x86
+    source $LINDOAPI_HOME/bin/linux64/lindoapivars.sh
 elif [ "${UNAME}" = "Darwin" ]; then
     PLATFORM=osx64x86
     #if [ "${MNAME}" = "arm64" ]; then
     #   PLATFORM=osx64arm
     #fi
+    source $LINDOAPI_HOME/bin/$PLATFORM/lindoapivars.sh
 fi
 if [ ! -z $TABOX_HOME ]; then
     remove_path $TABOX_HOME/bin/$PLATFORM 
 fi
 export PATH=$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LINDOAPI_HOME/bin/$PLATFORM:$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1
 export LUA_PATH="./?.lua;./share/lua/5.1/?.lua;./share/lua/5.1/?/init.lua"
 export LUA_CPATH="$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1/?.dll;$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1/?.so;$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1/?.dylib"
 
