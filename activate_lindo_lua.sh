2ad3e4b7861d7891fcde1b2d1d7b5cfc57172418
Mustafa Atlihan <mkatlihan@gmail.com>
2023-12-07 00:23:09 -0700

    darwin

--- activate_lindo_lua.sh
+++ activate_lindo_lua.sh
@@ -26,13 +26,17 @@ elif [ "${UNAME}" = "Darwin" ]; then
     #if [ "${MNAME}" = "arm64" ]; then
     #   PLATFORM=osx64arm
     #fi
-    source $LINDOAPI_HOME/bin/$PLATFORM/lindoapivars.sh
+    source $LINDOAPI_HOME/bin/$PLATFORM/lindoapivars.sh    
 fi
 if [ ! -z $TABOX_HOME ]; then
     remove_path $TABOX_HOME/bin/$PLATFORM 
 fi
 export PATH=$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LINDOAPI_HOME/bin/$PLATFORM:$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1
+if [ "${UNAME}" = "Darwin" ]; then
+    export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$LINDOAPI_HOME/bin/$PLATFORM:$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1
+else
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LINDOAPI_HOME/bin/$PLATFORM:$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1
+fi    
 export LUA_PATH="./?.lua;./share/lua/5.1/?.lua;./share/lua/5.1/?/init.lua"
 export LUA_CPATH="$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1/?.dll;$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1/?.so;$CURPATH/lib/$PLATFORM/systree/lib/lua/5.1/?.dylib"
 
