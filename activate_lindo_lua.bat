8bb93c2ac4f368b31a43f3766cde6c2037ba9ee5
Mustafa Atlihan <mkatlihan@gmail.com>
2024-02-27 00:29:46 +0300

    bat

--- activate_lindo_lua.bat
+++ activate_lindo_lua.bat
@@ -1,13 +1,16 @@
 @echo off
-set PLATFORM=%1
 
 rem Check for LINDOAPI_HOME and LINDOAPI_LICENSE_FILE variables
 if not defined LINDOAPI_HOME (
     echo LINDOAPI_HOME is not set. Please set the LINDOAPI_HOME environment variable.    
     exit /b 1
 )
-
-@call "%LINDOAPI_HOME%\bin\lindoapivars.bat
+if exist "%LINDOAPI_HOME%\bin\lindoapivars.bat" (
+    @call "%LINDOAPI_HOME%\bin\lindoapivars.bat"
+) else (
+    echo %LINDOAPI_HOME%\bin\lindoapivars.bat does not exist in the specified directory. Please check the installation of LINDO API.
+    exit /b 1
+)
 
 if not defined LINDOAPI_LICENSE_FILE (
     echo LINDOAPI_LICENSE_FILE is not set. Please check the installation of LINDO API.
@@ -27,6 +30,9 @@ if "%PLATFORM%"=="" (
         )
     )
 )
+REM Add x86 suffix if missing
+if "%PLATFORM%"=="win32" set "PLATFORM=win32x86"
+if "%PLATFORM%"=="win64" set "PLATFORM=win64x86"
 
 set "CURPATH=%~dp0"
 set LUA_PATH=.\?.lua;%CURPATH%\share\lua\5.1\?.lua;%CURPATH%\share\lua\5.1\?\init.lua;%LUA_PATH%
