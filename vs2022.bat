bad54e716ce393278598048c8da59ff10f88e64b
Mustafa Atlihan <mkatlihan@gmail.com>
2025-04-24 16:16:40 +0300

    vs2022

---
+++ vs2022.bat
@@ -0,0 +1,4 @@
+@echo off
+set TALIBDIR=c:\opt\xfin\ta-lib\0.4.0\ta-lib\c\
+set MUPARSERDIR=c:\opt\xparser\muParser\muparser_v2_2_3
+start "" "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
